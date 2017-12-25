f48ab2f3450065cfbc810fdce90a7a8ed16ef17e
Wong, Fu-Lian <turtle11311@gmail.com>
2017-12-25 23:23:49 +0800

    Add User DB

--- app/src/main/java/com/dwf/dontwastefood/MainActivity.java
+++ app/src/main/java/com/dwf/dontwastefood/MainActivity.java
@@ -1,5 +1,6 @@
 package com.dwf.dontwastefood;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -7,10 +8,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.dwf.dontwastefood.proto.UserModel;
+import com.dwf.dontwastefood.proto.UserModel.User;
+import com.google.protobuf.Message;
+import com.google.protobuf.TextFormat;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    public static List<User> userList;
+
+    private static final String TAG = "MainActivity";
 
     private TextView mTextMessage;
-
+    private AssetManager mAssetManager;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -34,11 +48,20 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private void loadData() throws IOException {
+        UserModel.UsersOrBuilder users = UserModel.Users.newBuilder();
+        InputStream input = mAssetManager.open("users.prototext");
+        InputStreamReader reader = new InputStreamReader(input);
+        TextFormat.Parser.newBuilder().build().merge(reader, (Message.Builder) users);
+        userList = users.getUserList();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAssetManager = getAssets();
         mTextMessage = findViewById(R.id.message);
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
